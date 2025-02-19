95c6523b3b691dd1735c4bc9610d8ea6d1635f9f
Data Recovery <datarecoveryman@gmail.com>
2025-02-19 02:19:48 -0500

    Python version, clean up the output from test-token-stream

--- test-token-stream.py
+++ test-token-stream.py
@@ -2,18 +2,19 @@
 from TokenStream import TokenStream
 
 test_statements = [
-    "PRINT X + 12345",
-    "PRINT \"Hello\"\nPRINT \"World\"",
+    "10 PRINT X + 12345",
+    "20 PRINT \"Hello\"\nPRINT \"World\"",
+    # Note the missing line numbers
     "PRINT 2*(10+X)\n",
     "IF X > 10 THEN\nPRINT Y!=X\nENDIF",
 ]
 for stmt in test_statements:
-    print("Statement:", stmt)
+    print("Statement:", stmt.rstrip())
     ts = TokenStream(stmt)
     #token = ts.next()
     #while token is not None:
     #    print("Token:", token)
     #    token = ts.next()
     for token in ts.all():
-        print(token)
+        print("  ", token)
     print("")
