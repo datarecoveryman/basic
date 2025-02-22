40eff7c3309929cef9e34abd743afc2126043255
Data Recovery <datarecoveryman@gmail.com>
2025-02-22 12:57:15 -0500

    Python version, clean up Runner and test-runner. Running well.

--- Runner.py
+++ Runner.py
@@ -2,26 +2,33 @@ import Parser
 import TokenStream
 
 class Runner:
-    def __init__(self, statements, vars_dict):
+    def __init__(self, statements, vars_dict, debug=False):
         self.statements = statements
         self.vars_dict = vars_dict
+        self.debug = debug
         # Given a list of statements, create a dictionary of line number -> statement
         self.lines = {}
         for stmt in statements:
             self.lines[stmt.ln] = stmt
+            self._debug_print(f"{stmt.ln}: {stmt}")
         # Use line_numbers as a zero-based index into the lines dictionary
         self.idx = 0
         self.line_numbers = sorted(self.lines.keys())[:]
-        #print("line_numbers", self.line_numbers)
-        #print("Runner: Lines in order:")
-        #for ln in self.line_numbers:
-        #    stmt = self.lines[ln]
-        #    print(f"{ln}: {stmt}")
+        self._debug_print("Runner: line_numbers", self.line_numbers)
+        if self.debug:
+            self._debug_print("Runner: Lines in order:")
+            for ln in self.line_numbers:
+                stmt = self.lines[ln]
+                self._debug_print(f"{ln}: {stmt}")
+    
+    def _debug_print(self, *args):
+        if self.debug:
+            print(*args)
     
     def next(self):
         p = self.peek()
         if p is None:
-            print("End of program.")
+            self._debug_print("End of program.")
             return False
         ln, stmt = p
         print(f"Execute line {ln}: {stmt}")
@@ -33,16 +40,22 @@ class Runner:
             else:
                 #print(f"Setting {stmt.var} to literal {stmt.expr}")
                 self.vars_dict[stmt.var] = stmt.expr.value
-            print(f"Assignment: set {stmt.var} to {self.vars_dict[stmt.var]}")
+            self._debug_print(f"Assignment: set {stmt.var} to {self.vars_dict[stmt.var]}")
         elif isinstance(stmt, Parser.CodeGoto):
-            print("GOTO", stmt.target)
-            if stmt.target not in self.line_numbers:
+            self._debug_print("GOTO", stmt.target)
+            if isinstance(stmt.target, TokenStream.Token):
+                #print("GOTO: getting value from token")
+                # If the target is a token, get its value
+                target_ln = stmt.target.value
+            else:
+                target_ln = stmt.target
+            if target_ln not in self.line_numbers:
                 print(f"Line {stmt.target} not found.")
                 return False
             # if idx is going to be incremented later, can I just subtract 1 here?
-            self.idx = self.line_numbers.index(stmt.target) - 1
+            self.idx = self.line_numbers.index(target_ln) - 1
         elif isinstance(stmt, Parser.CodeNoop):
-            print("REM Noop")
+            self._debug_print("REM Noop")
             pass
         elif isinstance(stmt, Parser.CodePrint):
             if isinstance(stmt.expr, Parser.Expression):
--- test-runner.py
+++ test-runner.py
@@ -1,45 +1,68 @@
-
 import Parser
 import TokenStream
 import Runner
 
-#+ "50 LET Y = Y + 1\n" \ # Requires expression support
-# Flipping 10 and 20 correctly fails due to undefined X.
-test_program_1 = "10 LET X = 100\n" \
-    + "20 LET Y = 777\n" \
-    + "40 REM comment\n" \
-    + "60 PRINT Y\n" \
-    + "80 LET Y = Q + 10\n" \
-    + "99 GOTO 60\n"
+def run_test(program, granular=True):
+    print("Test program:")
+    print(program.strip())
+    print("")
 
-test_program_2 = "10 LET X = 100\n" \
+    #print("Create token stream and parser...")
+    ts = TokenStream.TokenStreamSkippy(program)
+    p = Parser.ParserFF(ts, debug=False)
+    #print("")
+
+    #print("Parse the program into statements:")
+    #print("(Note: the line numbers are not sorted by Parser.)")
+    statements = []
+    if granular: # Granular
+        stmt = p.take_statement()
+        while stmt is not None:
+            statements.append(stmt)
+            #print("  Statement:", stmt)
+            stmt = p.take_statement()
+    else:
+        # all() won't get them as it parses if it crashes.
+        statements = p.all()
+        #for statement in statements:
+        #    print("  Statement:", statement)
+    #print("")
+
+    # Give statements to Runner
+    my_vars = {}
+    my_runner = Runner.Runner(statements, my_vars)
+    if True:
+        print("Program lines in-order:")
+        for ln in my_runner.line_numbers:
+            stmt = my_runner.lines[ln]
+            print(f"{ln}: {stmt}")
+        print("")
+
+    print("Running...")
+    max_ops = 15
+    ops = 0
+    keep_running = my_runner.next()
+    while ops < max_ops and keep_running:
+        ops += 1
+        if ops >= max_ops:
+            print("Max ops reached; stopping Runner early.")
+            break
+        keep_running = my_runner.next()
+    print("Vars:", my_vars)
+    print("")
+
+
+test_program_1 = "10 LET X = 100\n" \
     + "20 LET Y = X * 4\n"
 
-print("Test program:")
-print(test_program_2.strip())
-print("")
+#+ "50 LET Y = Y + 1\n" \ # Requires expression support
+# Flipping 10 and 20 correctly fails due to undefined X.
+test_program_2 = "20 LET X = 100\n" \
+    + "10 LET Y = 77\n" \
+    + "40 REM comment\n" \
+    + "60 PRINT Y\n" \
+    + "80 LET Y = Y + X\n" \
+    + "99 GOTO 60\n"
 
-print("Break program into statements:")
-ts = TokenStream.TokenStream(test_program_2)
-p = Parser.Parser(ts)
-statements = p.all()
-for statement in statements:
-    print(statement)
-print("")
-
-# Give statements to Runner
-my_vars = {}
-my_runner = Runner.Runner(statements, my_vars)
-if False:
-    print("Program lines in-order:")
-    for ln in my_runner.line_numbers:
-        stmt = my_runner.lines[ln]
-        print(f"{ln}: {stmt}")
-
-max_ops = 10
-ops = 0
-keep_running = my_runner.next()
-while ops < max_ops and keep_running:
-    ops += 1
-    keep_running = my_runner.next()
-print("Vars:", my_vars)
+run_test(test_program_1)
+run_test(test_program_2)
